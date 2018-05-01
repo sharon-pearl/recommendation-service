d75cdb69af7714b2635ee886fabaa3d17edaea8c
pearlin-sargunar <p.sargunar@everteam.com>
2018-05-01 20:01:19 +0530

    initial commit

---
+++ src/main/java/com/pearlin/whatflix/recommendation/server/RecommendationServer.java
@@ -0,0 +1,18 @@
+package com.pearlin.whatflix.recommendation.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServer {
+
+	private static final Logger logger = LoggerFactory.getLogger(RecommendationServer.class);
+
+	public static void main(String... args) {
+		SpringApplication.run(RecommendationServer.class, args);
+		logger.info("Recommendation server started");
+	}
+
+}
